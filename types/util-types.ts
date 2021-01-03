86736b8ce22b991a22d8cb017b444fb917c646d6
DarrenDanielDay <Darren_Daniel_Day@hotmail.com>
2021-01-04 01:27:03 +0800

    feat: Add some utility type.

--- types/util-types.ts
+++ types/util-types.ts
@@ -3,3 +3,12 @@ export type Inferior<A, B> = Superior<B, A>;
 export type Equal<A, B> = A extends B ? (B extends A ? true : false) : false;
 export type Union<A, B> = A | B;
 export type Intersection<A, B> = A & B;
+export type UnionToIntersection<U> = (
+  U extends any ? (_: U) => void : never
+) extends (_: infer T) => void
+  ? T
+  : never;
+
+export type ArrayItem<Arr extends any[]> = Arr extends (infer T)[] ? T : never;
+export type Promisefy<T> = T extends Promise<any> ? T : Promise<T>;
+export type UnPromisefy<T> = T extends Promise<infer R> ? R : T;
