1a64b3e3c073a1641d5a9c75db994e9adfe87811
Darren <leiyu.doctor@bytedance.com>
2021-02-06 02:25:00 +0800

    chore: Change types for `pipe`.

--- src/utils/function-tools/pipe.ts
+++ src/utils/function-tools/pipe.ts
@@ -1,37 +1,37 @@
 import { Func } from "../../types/util-types";
 import { asynchronize } from "./asynchronize";
-
-export type Pipe<Initial, Fns extends Func<[any], unknown>[]> = Fns extends [
+type AnySingleParamFunc = Func<[never], unknown>;
+export type Pipe<Initial, Fns extends AnySingleParamFunc[]> = Fns extends [
   infer First,
   ...infer Rest
 ]
   ? First extends (...args: infer P) => infer R
     ? [Initial] extends P
-      ? Rest extends Func<[any], unknown>[]
+      ? Rest extends AnySingleParamFunc[]
         ? Pipe<R, [...Rest]>
         : never
       : never
     : never
   : Initial;
 
-export function pipe<T, Params extends Func<[any], unknown>[]>(
+export function pipe<T, Params extends AnySingleParamFunc[]>(
   init: T,
   ...t: Params
 ): Pipe<T, Params> {
   let returnValue: unknown = init;
   for (const fn of t) {
-    returnValue = fn.call(undefined, returnValue);
+    returnValue = fn.call(undefined, returnValue as never);
   }
   return returnValue as never;
 }
 
-export async function pipeAsync<T, Params extends Func<[any], unknown>[]>(
+export async function pipeAsync<T, Params extends AnySingleParamFunc[]>(
   init: T,
   ...t: Params
 ): Promise<Pipe<T, Params>> {
   let returnValue: unknown = init;
   for (const fn of t) {
-    returnValue = await asynchronize(fn.bind(undefined, returnValue));
+    returnValue = await asynchronize(fn.bind(undefined, returnValue as never));
   }
   return returnValue as never;
 }
